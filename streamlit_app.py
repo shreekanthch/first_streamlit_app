65d5ac56de7121fb927783754196981d2d48a8e7
shreekanthch <106467717+shreekanthch@users.noreply.github.com>
2022-05-29 14:52:41 +0530

    Update streamlit_app.py
    created another header with name own smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale ,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
